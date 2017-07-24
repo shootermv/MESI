af7d5aafdd6037b82681c632cf05b22233912aae
shootermv <shootermv@gmail.com>
2017-07-24 23:33:27 +0300

    initDB rewiten to be more accurate

--- server/initDB.js
+++ server/initDB.js
@@ -2,15 +2,13 @@ var mongoose = require('mongoose')
     , Schema = mongoose.Schema
     , conn = 'mongodb://localhost/mydb';
 mongoose.connect(conn);
-/* role schema */
+
 console.log('connected');
-console.log('=====================');
-var RoleSchema = new Schema({
-    title: String,
-    bitMask: Number
-});
-var Role = mongoose.model('roles', RoleSchema);
-var User = require('./models/User.js')
+
+require('./models/User.js');
+var Role = mongoose.model('Role');
+var User = mongoose.model('User');
+
 var adminRole = new Role({
     title: 'admin',
     bitMask: 4
@@ -20,29 +18,13 @@ var userRole = new Role({
     bitMask: 2
 });
 
-adminRole.save(function (err) {
-    if (err) {
-        console.log('error happen:', err);
-    } else {
-        console.log('========admin role created=======');
-        User.addUser('admin', '12345', { title: 'admin' }, function () {
-
-            userRole.save(function (err) {
-                if (err) {
-                    console.log('error happen:', err);
-                } else {
-                    console.log('========user role created=======');
-                    User.addUser('shlomo', '12345', { title: 'user' }, function () { });
-                    User.addUser('Matt', '12345', { title: 'user' }, function () {
-                        User.addUser('Yoav', '12345', { title: 'user' }, function () {
-                            console.log('4 users created')
-                            process.exit(0);
-                        });
-                    })
-                }
-            });
 
 
-        });
-    }
-});
+adminRole.save().then(function (adminRole) {
+    return new User({ name: 'admin', password: '12345', role: adminRole._id }).save();
+}).then(function () {
+    return userRole.save();
+}).then(function (userRole) {
+    return new User({ name: 'shlomo', password: '12345', role: userRole._id }).save();
+    process.exit(0);
+})
